2c9d96a50cca91e205fbb25d041eefcd1d6a27c5
Oliver Daids <oddity007@gmail.com>
2013-08-14 13:41:06 -0400

    More cleaning

--- Parse.lua
+++ Parse.lua
@@ -137,159 +137,17 @@ local ParserRules =
 	{type = "end", action = function(nodes) return {} end}
 }
 
---[[local CommonActions = 
-{
-	passThroughFirstNode = function(nodes) return nodes[1] end,
-}
-
-local HumanFriendlyRules = 
-{
-	program = 
-	{
-		{"expression", "done", action = CommonActions.passThroughFirstNode}
-	},
-	
-	expression = 
-	{
-		{
-			"assignmentTargets", "yield", "chainedOrUnchainedClosedExpression",
-			action = 
-			function(arguments, function(nodes)
-				return {
-					type = "anonymous function",
-					arguments = reverse(nodes[1]),
-					body = nodes[3]
-				}
-			end
-		},
-		{
-			"identifier", "assignmentTargets", "assign", "chainedOrUnchainedClosedExpression", "terminal", "expression",
-			action = 
-			function(nodes)
-				return {
-					type = "function assignment",
-					functionName = nodes[1],
-					arguments = reverse(nodes[2]),
-					body = nodes[4],
-					followingContext = nodes[6]
-				}
-			end
-		},
-		{
-			"assignmentTarget",
-			"assign",
-			"chainedOrUnchainedClosedExpression",
-			"terminal",
-			"expression",
-			action = function(nodes)
-			return {type = "assignment", destination = nodes[1], source = nodes[3], followingContext = nodes[5]} end}
-	},
-	
-	{type = "expression", "assignmentTarget", "assign", "chainedOrUnchainedClosedExpression", "terminal", "expression", action = function(nodes) return {type = "assignment", destination = nodes[1], source = nodes[3], followingContext = nodes[5]} end},
-	{type = "expression", "chainedOrUnchainedClosedExpression", "single question mark", "chainedOrUnchainedClosedExpression", "terminal", "expression", action = function(nodes) return {type = "branch", condition = nodes[1], resultIfTrue = nodes[3], resultIfFalse = nodes[5]} end},
-	{type = "expression", "chainedExpression", action = function(nodes) return nodes[1] end},
-	{type = "expression", "closedExpression", action = function(nodes) return nodes[1] end},
-	
-	{type = "chainedOrUnchainedClosedExpression", "chainedExpression", action = function(nodes) return nodes[1] end},
-	{type = "chainedOrUnchainedClosedExpression", "closedExpression", action = function(nodes) return nodes[1] end},
-	
-	{type = "assignmentTargets", "assignmentTarget", "assignmentTargets", 
-		action = function(nodes)
-			local targets = nodes[2]
-			targets[#targets + 1] = nodes[1]
-			return targets
-		end
-	},
-	{type = "assignmentTargets", "assignmentTarget", "end", action = function(nodes) return {nodes[1]} end},
-	{type = "assignmentTarget", "eval", "closedExpression", action = function(nodes) return {type = "eval", expression = nodes[2]} end},
-	{type = "assignmentTarget", "double question mark", "closedExpression", action = function(nodes) return {type = "guard", expression = nodes[2]} end},
-	{type = "assignmentTarget", "identifier", action = function(nodes) return nodes[1] end},
-	{type = "assignmentTarget", "number", action = function(nodes) return nodes[1] end},
-	{type = "assignmentTarget", "string", action = function(nodes) return nodes[1] end},
-	{type = "assignmentTarget", "opening square bracket", "assignmentTargetListBody", "closing square bracket", action = function(nodes) return {type = "list", elements = reverse(nodes[2])} end},
-	{type = "assignmentTargetListBody", "assignmentTarget", "comma", "assignmentTargetListBody",
-		action = function(nodes)
-			local elements = nodes[3]
-			elements[#elements + 1] = nodes[1]
-			return elements
-		end
-	},
-	{type = "assignmentTargetListBody", "ellipsis", "assignmentTarget", 
-		action = function(nodes)
-			return {{type = "sublist", expression = nodes[2]}}
-		end
-	},
-	{type = "assignmentTargetListBody", "assignmentTarget", 
-		action = function(nodes)
-			return {nodes[1]}
-		end
-	},
-	{type = "assignmentTargetListBody", "end", action = function(nodes) return {} end},
-	
-	{type = "chainedExpression", "closedExpression", "chainedExpressionArguments", action = function(nodes) return {type = "call", callee = nodes[1], arguments = reverse(nodes[2])} end},
-	{type = "chainedExpressionArguments", "closedExpression", "chainedExpressionArguments", 
-		action = function(nodes)
-			local arguments = nodes[2]
-			arguments[#arguments + 1] = nodes[1]
-			return arguments
-		end
-	},
-	{type = "chainedExpressionArguments", "closedExpression", "end", action = function(nodes) return {nodes[1]} end},
-	
-	{type = "closedExpression", "opening parenthesis", "expression", "closing parenthesis", action = function(nodes) return nodes[2] end},
-	{type = "closedExpression", "identifier", action = function(nodes) return nodes[1] end},
-	{type = "closedExpression", "number", action = function(nodes) return nodes[1] end},
-	{type = "closedExpression", "string", action = function(nodes) return nodes[1] end},
-	
-	{type = "closedExpression", "opening square bracket", "expressionListBody", "closing square bracket", action = function(nodes) return {type = "list", elements = reverse(nodes[2])} end},
-	
-	{type = "expressionListBody", "expression", "comma", "expressionListBody",
-		action = function(nodes)
-			local elements = nodes[3]
-			elements[#elements + 1] = nodes[1]
-			return elements
-		end
-	},
-	{type = "expressionListBody", "ellipsis", "expression", 
-		action = function(nodes)
-			return {{type = "sublist", expression = nodes[2]}}
-		end
-	},
-	{type = "expressionListBody", "expression", 
-		action = function(nodes)
-			return {nodes[1]}
-		end
-	},
-	{type = "expressionListBody", "end", action = function(nodes) return {} end},
-	
-	{type = "end", action = function(nodes) return {} end}
-}]]
-
---[[local function GenerateParserFriendlyRules(humanFriendlyRules)
-	local parserFriendlyRules = {}
-	for type, branches in pairs(humanFriendlyRules) do
-		for _, branch in ipairs(branches) do
-			local parserFriendlyRule = {type = type, action = branch.action}
-			for i, item in ipairs(branch) do
-				parserFriendlyRule[i] = item
-			end
-			parserFriendlyRules[#parserFriendlyRules + 1] = parserFriendlyRule
-		end
-	end
-	return parserFriendlyRules
-end]]
-
-local function Parse(tokens, firstOverall, lastOverall, currentNonterminal, rules, HasRuleCache)
+local function Parse(tokens, firstOverall, lastOverall, currentNonterminal, rules, hasRuleCache)
 	local function HasRule(symbol)
-		local hasRule = HasRuleCache[symbol]
-		if hasRule ~= nil then return hasRule end
+		local hasRuleInCache = hasRuleCache[symbol]
+		if hasRuleInCache ~= nil then return hasRuleInCache end
 		for _, rule in ipairs(rules) do
 			if rule.type == symbol then
-				HasRuleCache[symbol] = true
+				hasRuleCache[symbol] = true
 				return true
 			end
 		end
-		HasRuleCache[symbol] = false
+		hasRuleCache[symbol] = false
 		return false
 	end
 	
@@ -307,7 +165,7 @@ local function Parse(tokens, firstOverall, lastOverall, currentNonterminal, rule
 					matchedNodes[matchedNodeCount] = {type = tokens[first].type, data = tokens[first].data}
 					first = first + 1
 				elseif HasRule(a) then
-					local node, count = Parse(tokens, first, last, a, rules, HasRuleCache)
+					local node, count = Parse(tokens, first, last, a, rules, hasRuleCache)
 					if count == nil then
 						matched = false
 						break
@@ -331,7 +189,7 @@ local function Parse(tokens, firstOverall, lastOverall, currentNonterminal, rule
 end
 
 return function(tokens)
-	local HasRuleCache = {}
-	local node = Parse(tokens, 1, #tokens, "program", ParserRules, HasRuleCache)
+	local hasRuleCache = {}
+	local node = Parse(tokens, 1, #tokens, "program", ParserRules, hasRuleCache)
 	return node
 end
